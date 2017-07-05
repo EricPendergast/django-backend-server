e38aa7948b51daf20ef2edf72ba7e466b8ca109d
Eric Pendergast <epender@umich.edu>
2017-07-05 10:57:40 +0800

    Remove some commented out code

--- entity/views.py
+++ entity/views.py
@@ -115,82 +115,10 @@ class EntityViewSet(viewsets.ViewSet):
             # Adding the dummy's fields to the actual entity
             entity.data_header = dummy.data_header
             entity.data = dummy.data
-            # id = entity.id
-            # print "Data of entity before save: %s" % entity.data
             entity.save()
-            #
-            # entity2 = Entity.objects.filter(id=id).first()
-            # print "Data of entity retrieved after save: %s" % entity2.data
-            # print "Data of entity reference after save: %s" % entity.data
-            #
-            # assert entity.id == entity2.id
-            # print str(type(entity.data)) + " " + str(type(entity2.data))
-            #
-            # entity2.data = entity.data
-            # assert entity2.data == entity.data
-            # entity2.save()
-            # entity.save()
         else:
             print "Invalid serializer"
         
-        
-        # print "Hello%s" % request.data['data_header']
-        # dataHeaderSerializers = [DataHeaderSerializer(data=item) for item in request.data['data_header']]
-        # if all(ser.is_valid() for ser in dataHeaderSerializers):
-        #     entity.data_header = [item.validated_data for item in dataHeaderSerializers]
-        
-        # if dataHeaderSerializer.is_valid():
-            # entity.data_header = dataHeaderSerializer.validated_data
-            # entity.data = entityDataSerializer.validated_data
-            # entity.data = entityDataJson
-        
-        # print "HERE"
-        # entityDataJson = util.csv_to_list_of_dictionaries(open(entity.source.file, 'r'), list=entity.data)
-        # entityDataSerializer =
-        # print "ASDF%sASDF" % entityDataJson
-        # entityDataSerializer = JSONParser().parse(BytesIO(entityDataJson))
-        
-            
-        
-        # entityData['data'] = []
-        # # putting the uploaded data into 'entityData'
-        # with open(entity.source.file, 'r') as file:
-        #
-        #     # assert request.data["file_format"] == "csv",\
-        #     # "TODO: allow use of tsv file format"
-        #
-        #     reader = csv.reader(file)
-        #
-        #     firstRow = None
-        #     for row in reader:
-        #         if firstRow is None:
-        #             firstRow = row
-        #         else:
-        #             assert len(row) == len(firstRow),\
-        #             "Header row and subsequent row(s) are not the same length"
-        #
-        #             # each item in the data list is refered to as a "data
-        #             # point"
-        #             dataPointDict = {}
-        #             for i in range(0, len(row)):
-        #                 dataPointDict[firstRow[i]] = row[i]
-        #
-        #             entityData['data'] += (dataPointDict,)
-        # print "DATA%s" % entityData
-        # entitySerializer = EntityDetailedSerializer(data=entityData, required=False, allow_null=True)
-        # if entitySerializer.is_valid():
-        #     entityNew = entitySerializer.validated_data
-        #     assert type(entityNew) is Entity
-        #     entityNew.save()
-        # else:
-        #     print entitySerializer.errors
-        #     print "Invalid serializer"
-        # print "DATA%s" % entityData
-        # entity.data = entityData['data']
-        
-        # entity.source = None
-        
-        # entity.save()
         return Response(status=200)
     
     # TODO: Don't leave this in
