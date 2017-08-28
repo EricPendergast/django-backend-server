afe0d524b3975504d4d033a1fb960fac1e16208d
kavimaluskam <kavimaluskam@gmail.com>
2017-08-28 18:49:27 +0800

    Dynamic RMF

--- eledata/core/entity_h2o_engine.py
+++ eledata/core/entity_h2o_engine.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from eledata.models.entity import *
+from dateutil.relativedelta import relativedelta
+# from h2o.estimators.random_forest import H2ORandomForestEstimator
 
 
 class EntityH2OEngine(object):
@@ -55,7 +57,7 @@ class EntityH2OEngine(object):
             {
                 "$group": {
                     "_id": "$data.User_ID",
-                    "sum_purchase_amount": {"$sum": "$data.Transaction_Value"},
+                    "clv": {"$sum": "$data.Transaction_Value"},
                 }
             }
         ]
@@ -74,9 +76,168 @@ class EntityH2OEngine(object):
         response = list(Entity.objects(group=cls.group, type='transaction').aggregate(*pipeline))
         response = pd.DataFrame(response)
         # post handling month base amount
-        response['sum_purchase_amount'] = response['sum_purchase_amount'] / month_diff
+        response['clv'] = response['clv'] / month_diff
         return response
 
-    # TODO: get_rmf_in_window
+    @classmethod
+    def get_rmf_in_window(cls, user_list=None, start_date=None, end_date=None):
+        month_diff = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month) + 1 if (
+            start_date and end_date) else 3
 
-    # TODO: get_allowance_in_window (or from settings)
+        pipeline = [
+            {"$unwind": "$data"},
+            {"$match": {}},
+            {
+                "$group": {
+                    "_id": "$data.User_ID",
+                    "monetary_amount": {"$sum": "$data.Transaction_Value"},
+                    "frequency": {"$sum": 1},
+                    "recency": {"$max": "$data.Transaction_Date"},
+                    "monetary_quantity": {"$sum": "$data.Transaction_Quantity"}
+                }
+            }
+        ]
+        match_obj = pipeline[1]["$match"]
+
+        user_list and match_obj.update({
+            "data.User_ID": {"$in": user_list}
+        })
+        start_date and end_date and match_obj.update({
+            "data.Transaction_Date": {
+                "$gte": start_date,
+                "$lte": end_date
+            }
+        })
+
+        response = list(Entity.objects(group=cls.group, type='transaction').aggregate(*pipeline))
+        response = pd.DataFrame(response)
+        # post handling month base amount
+        response['monetary_amount'] = response['monetary_amount'] / month_diff
+        response['monetary_quantity'] = response['monetary_quantity'] / month_diff
+        response['recency'] = end_date and (
+            map(lambda x: (end_date - x).days, response['recency'])) or (
+                                  map(lambda x: (datetime.datetime.now() - x).days, response['recency']))
+        return response
+
+    @classmethod
+    def get_allowance_in_window(cls, user_list=None, start_date=None, end_date=None):
+        pipeline = [
+            {"$unwind": "$data"},
+            {"$match": {}},
+            {
+                "$group": {
+                    "_id": {
+                        "user_id": "$data.User_ID",
+                        "transaction_date": {
+                            "year": {"$year": "$data.Transaction_Date"},
+                            "month": {"$month": "$data.Transaction_Date"},
+                            "day": {"$dayOfMonth": "$data.Transaction_Date"}
+                        }
+                    },
+                    "monetary_amount": {"$sum": "$data.Transaction_Value"},
+                },
+            },
+            {
+                "$group": {
+                    "_id": "$_id.user_id",
+                    "std_monetary_amount": {"$stdDevPop": "$monetary_amount"},
+                }
+            }
+        ]
+        match_obj = pipeline[1]["$match"]
+
+        user_list and match_obj.update({
+            "data.User_ID": {"$in": user_list}
+        })
+        start_date and end_date and match_obj.update({
+            "data.Transaction_Date": {
+                "$gte": start_date,
+                "$lte": end_date
+            }
+        })
+
+        response = list(Entity.objects(group=cls.group, type='transaction').aggregate(*pipeline))
+        response = pd.DataFrame(response)
+        return response
+
+    @classmethod
+    def mean_shift_standardize(cls, data):
+        for y in data.columns[1:]:
+            data[y] = abs(data[y] - data[y].mean()[0]) / data[y].sd()[0]
+        return data
+
+    @classmethod
+    def get_dynamic_rmf_in_window(cls, user_list=None, start_date=None, end_date=None, window_length=3):
+        month_diff = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month) + 1 if (
+            start_date and end_date) else 3
+        date_list = [end_date - relativedelta(months=i * window_length) for i in range(month_diff / window_length)]
+        date_list.append(start_date)
+        date_list = date_list[::-1]
+
+        branches = [
+            {"case": {"$and": [
+                {"$gte": [date_list[i + 1], "$data.Transaction_Date"]},
+                {"$lt": [date_list[i + 2], "$data.Transaction_Date"]},
+            ]}, "then": i} for i in range(len(date_list) - 2)
+        ]
+        branches.insert(0,
+                        {"case": {
+                            "$gte": [start_date, "$data.Transaction_Date"]
+                        }, "then": -1})
+        branches.append({"case": {
+            "lt": [end_date, "$data.Transaction_Date"]
+        }, "then": len(date_list) - 2})
+
+        for i in branches:
+            print i
+        # print(date_list)
+        pipeline = [
+            {"$unwind": "$data"},
+            {"$match": {}},
+            {
+                "$project": {
+                    "user_id": "$data.User_ID",
+                    "monetary_amount": {"$sum": "$data.Transaction_Value"},
+                    "recency": {"$max": "$data.Transaction_Date"},
+                    "monetary_quantity": {"$sum": "$data.Transaction_Quantity"},
+                    "transaction_date_group": {
+                        "$switch": {
+                            "branches": branches
+                        }
+                    }
+                }
+            },
+            {
+                "$group": {
+                    "_id": {
+                        "user_id": "$user_id",
+                        "transaction_date_group": "$transaction_date_group"
+                    },
+                    "monetary_amount": {"$sum": "$monetary_amount"},
+                    "frequency": {"$sum": 1},
+                    "recency": {"$max": "$recency"},
+                    "monetary_quantity": {"$sum": "$monetary_quantity"}
+                }
+            }
+        ]
+        match_obj = pipeline[1]["$match"]
+
+        user_list and match_obj.update({
+            "data.User_ID": {"$in": user_list}
+        })
+        start_date and end_date and match_obj.update({
+            "data.Transaction_Date": {
+                "$gte": start_date,
+                "$lte": end_date
+            }
+        })
+
+        response = list(Entity.objects(group=cls.group, type='transaction').aggregate(*pipeline))
+        response = pd.DataFrame(response)
+        # post handling month base amount
+        # response['monetary_amount'] = response['monetary_amount'] / month_diff
+        # response['monetary_quantity'] = response['monetary_quantity'] / month_diff
+        # response['recency'] = end_date and (
+        #     map(lambda x: (end_date - x).days, response['recency'])) or (
+        #                           map(lambda x: (datetime.datetime.now() - x).days, response['recency']))
+        return response
--- eledata/tests/test_core_h2o.py
+++ eledata/tests/test_core_h2o.py
@@ -1,21 +1,22 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
-from project import settings
-from eledata.core.entity_summary import *
-from eledata.core.entity_chart_summary import *
 from eledata.core.entity_h2o_engine import *
-import pandas as pd
+from eledata.core.entity_summary import *
 
 from django.test import Client
 from eledata.models.entity import Entity
 from eledata.models.users import User, Group
-from project import settings
 
-from eledata.util import from_json, to_json
+from eledata.util import from_json
 import h2o
-import platform
-import time
+
+# from project import settings
+# from eledata.util import to_json
+# from project import settings
+# import pandas as pd
+# import platform
+# import time
 
 
 class CoreH2OTestCase(TestCase):
@@ -68,6 +69,8 @@ class CoreH2OTestCase(TestCase):
         self.client.post("/users/login/", {"username": "admin", "password": "pass"})
 
     def test_first_clv_analysis(self):
+        # h2o.init()
+        # print(h2o.frames())
         with open(self.defaultTransactionFilename) as fp:
             ret = self.client.post('/entity/create_entity/',
                                    {'file': fp, 'entity': self.entityJSON1, 'isHeaderIncluded': True})
@@ -80,6 +83,7 @@ class CoreH2OTestCase(TestCase):
         user_group = Group.objects(name="dummy_group").get()
         entity_h2o_engine = EntityH2OEngine.init_engine(user_group)
 
+        # testing get_time_window
         time_range_response = EntityH2OEngine.get_time_window()
         self.assertEquals(time_range_response.keys(), [u'last_date', u'_id', u'first_date'])
         last_date, _id, first_date = [time_range_response[x] for x in list(time_range_response)]
@@ -87,33 +91,110 @@ class CoreH2OTestCase(TestCase):
         self.assertEquals(first_date, datetime.datetime(2016, 9, 4, 0, 0, 0))
         self.assertEquals(last_date, datetime.datetime(2017, 5, 3, 0, 0, 0))
 
+        # testing get_clv_in_window
         response = entity_h2o_engine.get_clv_in_window(start_date=first_date, end_date=last_date)
-        self.assertEquals(list(response.keys()), [u'_id', u'sum_purchase_amount'])
+        self.assertEquals(list(response.keys()), [u'_id', u'clv'])
         self.assertEquals(response['_id'].count(), 10)
-        self.assertEquals(round(response['sum_purchase_amount'].max(), 2), 42.75)
-        self.assertEquals(round(response['sum_purchase_amount'].mean(), 2), 24.29)
-        self.assertEquals(round(response['sum_purchase_amount'].min(), 2), 9.57)
+        self.assertEquals(round(response['clv'].max(), 2), 42.75)
+        self.assertEquals(round(response['clv'].mean(), 2), 24.29)
+        self.assertEquals(round(response['clv'].min(), 2), 9.57)
 
-        # Test take 2 mins (atm) so i am not running this (atm) YOLO
-        # def test_big_clv_analysis(self):
-        #     print(
-        #         self.WARNING + "Following test \"test_big_clv_analysis\" takes a relatively long time, please wait."
-        #         + self.ENDC)
-        #
-        #     with open(self.bigTransactionFilename) as fp:
-        #         ret = self.client.post('/entity/create_entity/',
-        #                                {'file': fp, 'entity': self.entityJSON1, 'isHeaderIncluded': False})
-        #
-        #     rid = from_json(ret.content)['entity_id']
-        #     self.client.post('/entity/%s/create_entity_mapped/' % rid,
-        #                      data=self.entityDataHeaderNoFileHeader, content_type="application/json",
-        #                      HTTP_X_REQUESTED_WITH='XMLHttpRequest')
-        #
-        #     user_group = Group.objects(name="dummy_group").get()
-        #     entity_h2o_engine = EntityH2OEngine.init_engine(user_group)
-        #     response = entity_h2o_engine.get_clv_in_window()
-        #     self.assertEquals(list(response.keys()), [u'_id', u'sum_purchase_amount'])
-        #     self.assertEquals(response['_id'].count(), 23570)
-        #     self.assertEquals(response['sum_purchase_amount'].max(), 13990.93)
-        #     self.assertEquals(response['sum_purchase_amount'].mean(), 106.08042554094187)
-        #     self.assertEquals(response['sum_purchase_amount'].min(), 0.0)
+        # ha1 = h2o.H2OFrame(python_obj=pd.DataFrame(response))
+        # print(response)
+
+        # testing get_rmf_in_window
+        response = entity_h2o_engine.get_rmf_in_window(start_date=first_date, end_date=last_date)
+        self.assertEquals(list(response.keys()),
+                          [u'_id', u'frequency', u'monetary_amount', u'monetary_quantity', u'recency'])
+        self.assertEquals(list(response['frequency']), [1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+        self.assertEquals([round(x, 2) for x in list(response['monetary_amount'])],
+                          [12.42, 9.57, 37.86, 9.92, 42.75, 20.85, 36.21, 21.92, 15.71, 35.65])
+        self.assertEquals([round(x, 2) for x in list(response['monetary_quantity'])],
+                          [1.00, 0.89, 1.56, 0.22, 2.00, 1.56, 0.67, 0.33, 1.33, 1.56])
+        self.assertEquals([round(x, 2) for x in list(response['recency'])],
+                          [138, 114, 175, 21, 241, 50, 40, 0, 108, 8])
+
+        # ha2 = h2o.H2OFrame(python_obj=pd.DataFrame(response))
+        # print(response)
+
+        response = entity_h2o_engine.get_allowance_in_window(start_date=first_date, end_date=last_date)
+        self.assertEquals(list(response.keys()), [u'_id', u'std_monetary_amount'])
+        self.assertEquals(list(response['std_monetary_amount']), [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+
+        # ha3 = h2o.H2OFrame(python_obj=pd.DataFrame(response))
+        # print(response)
+
+        print(entity_h2o_engine.get_dynamic_rmf_in_window(start_date=first_date, end_date=last_date))
+        # from h2o.estimators.random_forest import H2ORandomForestEstimator
+        # model = H2ORandomForestEstimator(
+        #     model_id="rf_covType_v2",
+        #     ntrees=50,
+        #     max_depth=30,
+        #     col_sample_rate_per_tree=0.95,
+        #     stopping_rounds=10,
+        #     stopping_tolerance=0.01,
+        #     score_each_iteration=True,
+        #     seed=3000000)
+        # print(h2o.frames())
+        # print(len(h2o.frames()['frames']))
+        # h2o.remove(ha1)
+        # h2o.remove(ha2)
+        # h2o.remove(ha3)
+        # print(h2o.frames())
+        # print(len(h2o.frames()['frames']))
+        # h2o.cluster().shutdown()
+
+    # Test take 2 mins (atm) so i am not running this (atm) YOLO
+    # def test_big_clv_analysis(self):
+    #     print ("--------" + str(datetime.datetime.now()) + "-------------")
+    #     print(
+    #         self.WARNING + "Following test \"test_big_clv_analysis\" takes a relatively long time, please wait."
+    #         + self.ENDC)
+    #
+    #     with open(self.bigTransactionFilename) as fp:
+    #         ret = self.client.post('/entity/create_entity/',
+    #                                {'file': fp, 'entity': self.entityJSON1, 'isHeaderIncluded': False})
+    #
+    #     rid = from_json(ret.content)['entity_id']
+    #     self.client.post('/entity/%s/create_entity_mapped/' % rid,
+    #                      data=self.entityDataHeaderNoFileHeader, content_type="application/json",
+    #                      HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+    #
+    #     user_group = Group.objects(name="dummy_group").get()
+    #     entity_h2o_engine = EntityH2OEngine.init_engine(user_group)
+    #
+    #
+    #     # testing get_time_window
+    #     time_range_response = EntityH2OEngine.get_time_window()
+    #     # self.assertEquals(time_range_response.keys(), [u'last_date', u'_id', u'first_date'])
+    #     last_date, _id, first_date = [time_range_response[x] for x in list(time_range_response)]
+    #
+    #     # self.assertEquals(first_date, datetime.datetime(2016, 9, 4, 0, 0, 0))
+    #     # self.assertEquals(last_date, datetime.datetime(2017, 5, 3, 0, 0, 0))
+    #
+    #     # testing get_clv_in_window
+    #     # response = entity_h2o_engine.get_clv_in_window(start_date=first_date, end_date=last_date)
+    #     # print(response)
+    #     # self.assertEquals(list(response.keys()), [u'_id', u'sum_purchase_amount'])
+    #     # self.assertEquals(response['_id'].count(), 23570)
+    #     # self.assertEquals(response['sum_purchase_amount'].max(), 13990.93)
+    #     # self.assertEquals(response['sum_purchase_amount'].mean(), 106.08042554094187)
+    #     # self.assertEquals(response['sum_purchase_amount'].min(), 0.0)
+    #
+    #     # testing get_rmf_in_window
+    #     # response = entity_h2o_engine.get_rmf_in_window(start_date=first_date, end_date=last_date)
+    #     # print(response)
+    #     # self.assertEquals(list(response.keys()),
+    #     #                   [u'_id', u'frequency', u'monetary_amount', u'monetary_quantity', u'recency'])
+    #     # self.assertEquals(list(response['frequency']), [1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    #     # self.assertEquals([round(x, 2) for x in list(response['monetary_amount'])],
+    #     #                   [12.42, 9.57, 37.86, 9.92, 42.75, 20.85, 36.21, 21.92, 15.71, 35.65])
+    #     # self.assertEquals([round(x, 2) for x in list(response['monetary_quantity'])],
+    #     #                   [1.00, 0.89, 1.56, 0.22, 2.00, 1.56, 0.67, 0.33, 1.33, 1.56])
+    #     # self.assertEquals([round(x, 2) for x in list(response['recency'])],
+    #     #                   [138, 114, 175, 21, 241, 50, 40, 0, 108, 8])
+    #
+    #     response = entity_h2o_engine.get_allowance_in_window(start_date=first_date, end_date=last_date)
+    #     print(response)
+    #     # self.assertEquals(list(response.keys()), [u'_id', u'sum_purchase_amount'])
+    #     print ("--------" + str(datetime.datetime.now()) + "-------------")
