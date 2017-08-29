63d4b61ea9613a8e9c7f3364da962e68aa5bcf0c
kavimaluskam <kavimaluskam@gmail.com>
2017-08-29 13:05:46 +0800

    Travis, H2O Core Updated

--- eledata/tests/test_core_h2o.py
+++ eledata/tests/test_core_h2o.py
@@ -83,8 +83,13 @@ class CoreH2OTestCase(TestCase):
         user_group = Group.objects(name="dummy_group").get()
         entity_h2o_engine = EntityH2OEngine.init_engine(user_group)
 
+        # testing get_user_list
+        response = entity_h2o_engine.get_transaction_based_user_list()
+        self.assertEquals(list(response), [u'_id'])
+        self.assertEquals(len(response), 10)
+
         # testing get_time_window
-        time_range_response = EntityH2OEngine.get_time_window()
+        time_range_response = entity_h2o_engine.get_time_window()
         self.assertEquals(time_range_response.keys(), [u'last_date', u'_id', u'first_date'])
         last_date, _id, first_date = [time_range_response[x] for x in list(time_range_response)]
 
@@ -108,11 +113,11 @@ class CoreH2OTestCase(TestCase):
                           [u'_id', u'frequency', u'monetary_amount', u'monetary_quantity', u'recency'])
         self.assertEquals(list(response['frequency']), [1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
         self.assertEquals([round(x, 2) for x in list(response['monetary_amount'])],
-                          [12.42, 9.57, 37.86, 9.92, 42.75, 20.85, 36.21, 21.92, 15.71, 35.65])
+                          [42.75, 12.42, 15.71, 37.86, 36.21, 9.92, 9.57, 21.92, 20.85, 35.65])
         self.assertEquals([round(x, 2) for x in list(response['monetary_quantity'])],
-                          [1.00, 0.89, 1.56, 0.22, 2.00, 1.56, 0.67, 0.33, 1.33, 1.56])
+                          [2.0, 1.0, 1.33, 1.56, 0.67, 0.22, 0.89, 0.33, 1.56, 1.56])
         self.assertEquals([round(x, 2) for x in list(response['recency'])],
-                          [138, 114, 175, 21, 241, 50, 40, 0, 108, 8])
+                          [241.0, 138.0, 108.0, 175.0, 40.0, 21.0, 114.0, 0.0, 50.0, 8.0])
 
         # ha2 = h2o.H2OFrame(python_obj=pd.DataFrame(response))
         # print(response)
@@ -163,7 +168,7 @@ class CoreH2OTestCase(TestCase):
     #
     #     user_group = Group.objects(name="dummy_group").get()
     #     entity_h2o_engine = EntityH2OEngine.init_engine(user_group)
-    #
+    #     # print("Length of user: " + str(len(entity_h2o_engine.get_user_list())))
     #
     #     # testing get_time_window
     #     time_range_response = EntityH2OEngine.get_time_window()
@@ -195,7 +200,7 @@ class CoreH2OTestCase(TestCase):
     #     # self.assertEquals([round(x, 2) for x in list(response['recency'])],
     #     #                   [138, 114, 175, 21, 241, 50, 40, 0, 108, 8])
     #
-    #     response = entity_h2o_engine.get_allowance_in_window(start_date=first_date, end_date=last_date)
+    #     response = entity_h2o_engine.get_dynamic_rmf_in_window(start_date=first_date, end_date=last_date)
     #     print(response)
     #     # self.assertEquals(list(response.keys()), [u'_id', u'sum_purchase_amount'])
     #     print ("--------" + str(datetime.datetime.now()) + "-------------")
