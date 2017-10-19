e400ee4096ab96329ec840d03ece27151d4ba8dc
tc-livelyimpact <32475440+tc-livelyimpact@users.noreply.github.com>
2017-10-19 15:40:44 +0800

    To generate mockup csv data files

--- misc/test_files/create_mockdata.py
+++ misc/test_files/create_mockdata.py
@@ -12,21 +12,22 @@ generator = Faker('en_US')
 num_customers = random.randint(400, 1000)
 num_transaction = num_customers * random.randint(0, 40)
 
-with open('transaction_records.csv', 'w') as transaction_file:
-    transaction_file.write('ID,User_ID,Transaction_Date,Transaction_Value,Product_Measure,Product_Size,Brand,Category,Chain\n')
+with open('transaction_records.tsv', 'w') as transaction_file:
+    transaction_file.write('ID\tUser_ID\tTransaction_Date\tTransaction_Quantity\tTransaction_Value\tProduct_Measure\tProduct_Size\tBrand\tCategory\tChain\n')
     transaction_id = 0
     measures = ['Box(s)', 'Unit(s)']
     brands = ['Vinda', 'Airwaves', 'Nike', 'Whatever']
     categories = ['groceries', 'shoes', 'snacks', 'beverage']
+    countries = ['China', 'Japan', 'Hong Kong', 'Korea']
 
-    with open('customer_records.csv', 'w') as customer_file:
-        customer_file.write('ID,Contact_Number,Contact_Email,Last_Name,First_Name,Display_Name,Age,Gender,Address,Create_Date,Last_Modified_Date,Country\n')
+    with open('customer_records.tsv', 'w') as customer_file:
+        customer_file.write('ID\tContact_Number\tContact_Email\tLast_Name\tFirst_Name\tDisplay_Name\tAge\tGender\tAddress\tCreate_Date\tLast_Modified_Date\tCountry\n')
         for i in range(1, num_customers+1):
             gender = 'Male' if random.randint(0,2) == 0 else 'Female'
             create_date = get_past_date(generator, start_date=datetime.date.today()-datetime.timedelta(days=730))
             modified_date = get_past_date(generator, start_date=create_date)
 
-            customer_file.write('{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10}\n'.format
+            customer_file.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\t{10}\t{11}\n'.format
                 (
                     i,
                     generator.phone_number(),
@@ -35,10 +36,11 @@ with open('transaction_records.csv', 'w') as transaction_file:
                     generator.first_name_male() if gender == 'Male' else generator.first_name_female(),
                     generator.user_name(),
                     random.randint(14, 75),
+                    gender,
                     generator.address().replace('\n', ', '),
                     create_date,
                     modified_date,
-                    generator.country()
+                    countries[random.randint(0, len(countries)-1)]
                 )
             )
 
@@ -46,7 +48,7 @@ with open('transaction_records.csv', 'w') as transaction_file:
                 transaction_id += 1
                 quantity = random.randint(1, 20)
 
-                transaction_file.write('{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}\n'.format
+                transaction_file.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\n'.format
                     (
                         transaction_id,
                         i,
